74675a91b450aeea968a915a6067dad8936f7525
martin <martonlineca@gmail.com>
2019-04-09 20:36:07 -0400

    array

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array{
+	public static void main(String[] args){
+		int[] intarray;
+		intarray = new int[3];
+		intarray[0] = 43;
+		intarray[1] = 64;
+		intarray[2] = 54;
+		System.out.println(intarray[2]);
+	}
+}
