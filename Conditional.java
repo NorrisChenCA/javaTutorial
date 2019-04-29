e539143b13310c4bc7b220358d91c7e02d4b26bb
martin <martonlineca@gmail.com>
2019-04-29 17:37:04 -0400

    Conditional

---
+++ Conditional.java
@@ -0,0 +1,11 @@
+public class Conditional{
+	public static void main(String[] args){
+	int a = 4;
+	int b = 6;
+	if((a == 4)||(b == 6))
+	System.out.println("true");
+	else{
+		System.out.println("false");
+		}
+	}
+}
