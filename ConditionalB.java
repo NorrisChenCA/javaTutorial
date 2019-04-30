2cfc746892566b315dd8e18408a6264a5592a82c
martin <martonlineca@gmail.com>
2019-04-30 17:19:12 -0400

    ConditionalB

---
+++ ConditionalB.java
@@ -0,0 +1,11 @@
+public class ConditionalB{
+	public static void main(String[] args){
+		int a = 5;
+		int b = 6;
+		if ((a == 5)&&(b == 6))
+			System.out.println("true");
+		else{
+			System.out.println("false");
+		}
+	}
+}
