d6f5a2e8f76954f658324e4a5759c8ace965e49a
martin <martonlineca@gmail.com>
2019-05-01 18:22:16 -0400

    ConditionalC

---
+++ CoditionalC.java
@@ -0,0 +1,13 @@
+class ConditionalC{
+	public static void main(String[] args){
+		int a = 10;
+		int b = 4;
+		int c = 5;
+		int d = 7;
+		if((a == 10) || (b == 4) && (c == 5) || (d == 7))
+		System.out.println("true");
+		else{
+			System.out.println("false");
+		}
+	}
+}
